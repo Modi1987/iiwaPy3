87332e90d7b0718c1687becc81c27d517aa86ee5
Modi1987 <ms@uc.pt>
2021-01-24 14:55:30 +0000

    Adding a menu-bar and video-tutorials link

--- python_client/Tutorial_control_iiwa_from_gui.py
+++ python_client/Tutorial_control_iiwa_from_gui.py
@@ -12,12 +12,29 @@ Example for controlling iiwa using GUI from an external PC
 
 from tkinter import *
 from iiwaPy3 import iiwaPy3
+import webbrowser
 import math
+from time import sleep
 
 class App:
     def __init__(self):
         root = Tk()
         self.root=root
+        # Add a menu bar
+        my_menu=Menu(root)
+        root.config(menu=my_menu)
+        # Add robot menu
+        robot_menu = Menu(my_menu)
+        robot_menu.add_command(label='Connect', command=self.connectFun)
+        robot_menu.add_command(label='disconnnect', command=self.disconnectFun)
+        robot_menu.add_command(label='exit', command=self.close_program)
+        my_menu.add_cascade(label='Robot',menu=robot_menu)
+        # Add a help menu
+        help_menu = Menu(my_menu)
+        help_menu.add_command(label='online video tutorials',command=self.online_Help)
+        help_menu.add_command(label='About', command = self.info_Window)
+        my_menu.add_cascade(label='help',menu=help_menu)
+        # Make the interface
         self.IP_of_robot= StringVar()
         self.stateMessage=StringVar()
         self.connection_state=False
@@ -80,7 +97,8 @@ class App:
         
         # Add the STATE MESSAGE textbox
         Label(mainFrame, text=" Msg").grid(row=8, column=0,sticky=W) 
-        msgTxt=Entry(mainFrame,textvariable=self.stateMessage,width="40").grid(row=8, column=1,columnspan=4,rowspan=1,sticky=W)       
+        self.msgTxt=Entry(mainFrame,textvariable=self.stateMessage,width="40")
+        self.msgTxt.grid(row=8, column=1,columnspan=4,rowspan=1,sticky=W)       
         #print(msgTxt.winfo_width())      
         self.stateMessage.set("Not connected")
         
@@ -91,7 +109,7 @@ class App:
         S.config(command=T.yview)
         self.commandsText=T
         self.commandsScroll=S
-        root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        root.protocol("WM_DELETE_WINDOW", self.close_program)
         try:
             root.mainloop()
         except:
@@ -101,7 +119,7 @@ class App:
                 self.disconnectFun()
             
         
-    def on_closing(self):
+    def close_program(self):
         if self.connection_state==False:
             self.root.destroy()
         else:
@@ -112,31 +130,33 @@ class App:
         # Check if lready connected to the robot
         if self.connection_state:
             message="Already connected to the robot on IP: "+self.IP_of_robot.get()
-            self.printMessage(message)
+            self.printMessage(message,'#0000ff')
             return
         # If the program made it to here, then there is no connection yet
         message="Connecting to robot at ip: "+self.IP_of_robot.get()
-        self.printMessage(message)
+        self.printMessage(message,'#00ff00')
+        sleep(0.1)
         try:
             self.iiwa=iiwaPy3(self.IP_of_robot.get())
+            jpos=self.iiwa.getJointsPos()
+            for i in range(7):
+                temp=jpos[i]*180/math.pi
+                self.jText[i].set(str(temp))       
+                # print "value of joint angle "+str(i)
+                # print self.jText[i].get()
             self.connection_state=True
             message="Connection established succefully"
-            self.printMessage(message)
+            self.printMessage(message,'#0000ff')
         except:
             message="Error, could not connect at the specified IP"
-            self.printMessage(message)
+            self.printMessage(message,'#ff0000')
             return            
-        jpos=self.iiwa.getJointsPos()
-        for i in range(7):
-            temp=jpos[i]*180/math.pi
-            self.jText[i].set(str(temp))       
-            # print "value of joint angle "+str(i)
-            # print self.jText[i].get()
+
             
     def disconnectFun(self):
         # Check if there is an active connection
         if self.connection_state==False:
-            message="Already off line"
+            message="Already off-line"
             self.printMessage(message)
             return
         # If made it to here, then there is an active connection
@@ -235,12 +255,42 @@ class App:
             self.commandsText.insert(END, item)
         self.commandsText.see(END)
         
-    def printMessage(self,message):
+    def printMessage(self,message, color = '#000000'):
         # print message
         self.stateMessage.set(message)
+        self.msgTxt.config(fg = color)
+
+    def online_Help(self):
+        daURL='https://www.youtube.com/watch?v=QkUe8JIs63A&list=PLz558OYgHuZdRoxkqQ-M9LOdksZnEWbKq&index=2&t=0s'
+        webbrowser.open(daURL, new=2)
+
+    def info_Window(self):
+        window=Toplevel()
+        daFrame=LabelFrame(window)
+        daFrame.pack()
+        txt='Copyright: Mohammad Safeea, Jan-2021 \n'
+        txt=txt+'Tutorial script for controlling KUKA iiwa robot \n'
+        txt=txt+'from Python3 GUI.\n'
+        txt=txt+'==============================.\n'
+        txt=txt+'For more info check\n'
+        txt=txt+'https://github.com/Modi1987/iiwaPy3\n';
+        txt=txt+'==============================.\n'
+        label=Label(daFrame,text=txt)
+        label.pack()
+        tempButton=Button(daFrame,text='ok', command=lambda: window.destroy())
+        tempButton.pack()
+        w_width=window.winfo_reqwidth()
+        w_height=window.winfo_reqheight()
+        s_width=window.winfo_screenwidth()
+        s_height=window.winfo_screenheight()
+        x_pos=int((s_width-w_width)/2)
+        y_pos=int((s_height-w_height)/2)
+        window.geometry("+{}+{}".format(x_pos,y_pos))
+        window.lift()
+        window.attributes('-topmost', True) 
              
 Appliaction=App()
- 
+
 
 
 
